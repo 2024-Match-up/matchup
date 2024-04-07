0f961fde7a5aa2374acb82afefa33074803684a1
jungeun <angal2310@tukorea.ac.kr>
2024-04-07 20:14:25 +0900

    FE/feat : 로그인 페이지, 마이페이지

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:matchup/screens/login_screen.dart';
+import 'package:matchup/screens/login/login_screen.dart';
 
 void main() {
   runApp(MyApp());
