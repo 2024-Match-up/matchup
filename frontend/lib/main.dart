cf7b79302184742dd076057128af30ac7b08a4ef
jungeun <angal2310@tukorea.ac.kr>
2024-05-20 03:40:33 +0900

    refactor: 코드 주석 정리

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,31 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:camera/camera.dart';
-// import 'package:matchup/screens/login/login_screen.dart';
-// import 'package:matchup/screens/camera/body_scan.dart'; 
-// import 'package:intl/intl.dart';
-// import 'package:intl/date_symbol_data_local.dart';
-
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   initializeDateFormatting('ko_KR', null);
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'match up!',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: LoginScreen(),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
