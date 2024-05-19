a4309568b23be21abd2f6d994c2087fff1ee28d1
jungeun <angal2310@tukorea.ac.kr>
2024-05-20 02:07:49 +0900

    feat: 백엔드 연동 및 클릭 시 사진 확대

--- frontend/lib/screens/camera/gallery_screen.dart
+++ frontend/lib/screens/camera/gallery_screen.dart
@@ -1,9 +1,160 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:provider/provider.dart';
+// import 'package:matchup/models/UserProvider.dart'; 
 
-class GalleryScreen extends StatelessWidget {
-  final List<String> imageList = [
-    'lib/assets/images/logo.jpg',
-  ];
+// class GalleryScreen extends StatefulWidget {
+//   @override
+//   _GalleryScreenState createState() => _GalleryScreenState();
+// }
+
+// class _GalleryScreenState extends State<GalleryScreen> {
+//   List<String> imageList = [];
+//   bool isLoading = true;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchImages();
+//   }
+
+//   Future<void> fetchImages() async {
+//     final String baseUrl = 'http://172.30.1.87:8000/api/v1';
+//     final userProvider = Provider.of<UserProvider>(context, listen: false);
+//     final token = userProvider.accessToken;
+
+//     if (token == null) {
+//       print('Token is null');
+//       setState(() {
+//         isLoading = false;
+//       });
+//       return;
+//     }
+
+//     try {
+//       final response = await http.get(
+//         Uri.parse("$baseUrl/health/image"),
+//         headers: {
+//           'Authorization': 'Bearer $token',
+//         },
+//       );
+
+//       if (response.statusCode == 200) {
+//         final data = json.decode(response.body);
+//         setState(() {
+//           imageList = [data['front_url'], data['side_url']];
+//           isLoading = false;
+//         });
+//       } else {
+//         throw Exception('Failed to load images');
+//       }
+//     } catch (e) {
+//       print('Error fetching images: $e');
+//       setState(() {
+//         isLoading = false;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final int crossAxisCount = 3;
+//     final double gridSpacing = 4.0;
+//     final double borderSize = 1.0;
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('갤러리'),
+//         backgroundColor: Color(0xFFBBBBEE),
+//       ),
+//       body: isLoading
+//           ? Center(child: CircularProgressIndicator())
+//           : GridView.builder(
+//               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                 crossAxisCount: crossAxisCount,
+//                 crossAxisSpacing: gridSpacing,
+//                 mainAxisSpacing: gridSpacing,
+//               ),
+//               itemCount: imageList.length,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return Container(
+//                   decoration: BoxDecoration(
+//                     border: Border.all(
+//                       color: Colors.grey,
+//                       width: borderSize,
+//                     ),
+//                   ),
+//                   child: Image.network(
+//                     imageList[index],
+//                     fit: BoxFit.cover,
+//                   ),
+//                 );
+//               },
+//             ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import 'package:matchup/models/UserProvider.dart'; // UserProvider 경로 수정
+import 'image_viewer.dart'; // ImageViewer 경로 추가
+
+class GalleryScreen extends StatefulWidget {
+  @override
+  _GalleryScreenState createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  List<String> imageList = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImages();
+  }
+
+  Future<void> fetchImages() async {
+    final String baseUrl = 'http://172.30.1.87:8000/api/v1';
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final token = userProvider.accessToken;
+
+    if (token == null) {
+      print('Token is null');
+      setState(() {
+        isLoading = false;
+      });
+      return;
+    }
+
+    try {
+      final response = await http.get(
+        Uri.parse("$baseUrl/health/image"),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          imageList = [data['front_url'], data['side_url']];
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load images');
+      }
+    } catch (e) {
+      print('Error fetching images: $e');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,37 +162,48 @@ class GalleryScreen extends StatelessWidget {
     final double gridSpacing = 4.0;
     final double borderSize = 1.0;
 
-    final int totalCells = crossAxisCount * 10; 
-
     return Scaffold(
       appBar: AppBar(
         title: Text('갤러리'),
         backgroundColor: Color(0xFFBBBBEE),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: crossAxisCount,
-          crossAxisSpacing: gridSpacing,
-          mainAxisSpacing: gridSpacing,
-        ),
-        itemCount: totalCells,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey, 
-                width: borderSize,
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount,
+                crossAxisSpacing: gridSpacing,
+                mainAxisSpacing: gridSpacing,
               ),
+              itemCount: imageList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ImageViewer(
+                          imageList: imageList,
+                          initialIndex: index,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: borderSize,
+                      ),
+                    ),
+                    child: Image.network(
+                      imageList[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
             ),
-            child: index < imageList.length
-                ? Image.asset(
-                    imageList[index],
-                    fit: BoxFit.cover,
-                  )
-                : null, 
-          );
-        },
-      ),
     );
   }
 }
