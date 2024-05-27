309ea8f540288b74c217a0d733fa622411e959c4
jungeunyooon <angal2310@tukorea.ac.kr>
2024-05-28 00:46:52 +0900

    feat: 파일추가

--- backend/session/schemas.py
+++ backend/session/schemas.py
@@ -1,14 +1,19 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+from models import Exercise
 
 class SessionBase(BaseModel):
     exercise_id: int
     user_id: int
+    date: datetime
 
 class SessionCreate(SessionBase):
     pass
 
 class Session(SessionBase):
     id: int
+    exercise: Exercise
 
     class Config:
         orm_mode = True
